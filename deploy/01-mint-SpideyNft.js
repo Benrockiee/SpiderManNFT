a62610e32a608632196561c7011c2e4d71bc73ba
Benrockiee <benrockiee@gmail.com>
2022-11-17 08:02:34 -0800

    first commit

--- deploy/01-mint-SpideyNft.js
+++ deploy/01-mint-SpideyNft.js
@@ -8,6 +8,6 @@ module.exports = async ({ getNamedAccounts }) => {
     const spideyNft = await ethers.getContract("SpideyNft", deployer)
     const spideyMintTx = await spideyNft.mintNft()
     await spideyMintTx.wait(1)
-    //console.log(`Spidey NFT index 0 tokenURI: ${await spideyNft.tokenURI(0)}`)
+    console.log(`Spidey NFT index 0 tokenURI: ${await spideyNft.tokenURI(0)}`)
 }
 module.exports.tags = ["all", "mint"]
