1ef86bba92d8a8b1bcda8eca26e82be1335ea947
Benjamin izuchukwu umeike <benrockiee@gmail.com>
2022-11-28 09:09:22 -0800

    Update helper-hardhat-config.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -2,8 +2,7 @@ const networkConfig = {
     31337: {
         name: "localhost",
     },
-    // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
-    // Default one is ETH/USD contract on Kovan
+    
     5: {
         name: "goerli",
     },
